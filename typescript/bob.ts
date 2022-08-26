365f120bd09c8d50220f8ef9c850d786c2ee3aa1
Daniel Castro <castrodd@gmail.com>
2022-08-26 12:04:20 -0700

    bob

---
+++ typescript/bob.ts
@@ -0,0 +1,37 @@
+export function hey(message: string): string {
+    const trimmedMessage: string = message.trim()
+    const responses = {
+      'question': 'Sure.',
+      'all_caps': 'Whoa, chill out!',
+      'all_caps_question': 'Calm down, I know what I\'m doing!',
+      'blank': 'Fine. Be that way!',
+      'default': 'Whatever.'
+    }
+  
+    const all_caps = (): boolean => {
+      const regexCaps = /[A-Z]/g
+      const regexLowerCase = /[a-z]/g
+      const hasCaps = trimmedMessage.match(regexCaps)?.length ? true : false
+      const hasNoLowerCase = trimmedMessage.match(regexLowerCase)?.length ? false : true
+      return hasCaps && hasNoLowerCase
+    }
+    
+    if (trimmedMessage.endsWith('?') && all_caps()) {
+        return responses['all_caps_question']
+    }
+
+    if (trimmedMessage.endsWith('?')) {
+      return responses['question']
+    }
+  
+    if (all_caps()) {
+      return responses['all_caps']
+    }
+  
+    if (!trimmedMessage) {
+      return responses['blank']
+    }
+  
+    return responses['default']
+  }
+  
