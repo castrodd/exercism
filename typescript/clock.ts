cb3e0920e12aadea5917e3d4d3faf79a7e57f8ed
Daniel Castro <castrodd@gmail.com>
2022-10-29 14:27:44 -0700

    Clock

---
+++ typescript/clock.ts
@@ -0,0 +1,49 @@
+export class Clock {
+  hour: number;
+  minute: number;
+  
+  constructor(hour: number, minute?: number) {
+    const safeMinute = minute ? minute : 0
+    const rawMinute = safeMinute % 60
+    this.minute = rawMinute >= 0 ? rawMinute : 60 + rawMinute
+
+    const rawHour = (hour + Math.floor(safeMinute/60))%24
+    this.hour = rawHour >= 0 ? rawHour : 24 + rawHour
+  }
+
+  public toString(): string {
+    const hour = this.hour < 10 ? `0${this.hour}` : this.hour.toString()
+    const minute = this.minute < 10 ? `0${this.minute}` : this.minute.toString()
+    return `${hour}:${minute}`
+  }
+
+  public plus(minutes: number): Clock {
+    const totalMinutes = this.minute + minutes
+    this.hour = (this.hour + Math.floor(totalMinutes/60))%24
+    this.minute = (totalMinutes)%60
+    return new Clock(this.hour, this.minute)
+  }
+
+  public minus(minutes: number): Clock {
+    const displacement = (this.hour - Math.floor(minutes/60))%24
+    if (displacement > 0) {
+      this.hour = displacement
+    } else {
+      this.hour = 24 + displacement
+    }
+
+    minutes = minutes%60
+    if (this.minute - minutes >= 0) {
+      this.minute = this.minute - minutes
+    } else {
+       this.minute = 60 + (this.minute - minutes)%60
+       this.hour -= 1
+    }
+    
+    return new Clock(this.hour, this.minute)
+  }
+
+  public equals(other: Clock): Boolean {
+    return this.toString() == other.toString()
+  }
+}
